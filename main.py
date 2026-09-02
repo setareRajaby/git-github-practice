da2f72fc2840b285b36ceeb2710e28d9235d450e
Setare <setarerajaby4@gmail.com>
2026-09-03 03:23:59 +0330

    update smash practice

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
 print("Welcome to the project")
 print("Version 2")
-print("New feature added")
-print("Change made on GitHub")
-print("Fetch practice")
-print("Second fetch practice")
-
-
-print("Master update")
-litle printttttttttt
-
-
-
-
-print("First squash change")
-print("Second squash change")
